3934a31a8714a8c6c3466e2ee4fadfeea8bb407b
Yuvraj9769 <130125820+Yuvraj9769@users.noreply.github.com>
2024-03-13 18:57:01 +0530

    Update wow.js

--- wow.js
+++ wow.js
@@ -13,59 +13,72 @@ let s_container = document.getElementsByClassName("s-container")[0];
 let shopby_corncern = document.getElementsByClassName("shopby-corncern")[0];
 let section_4_container_sub = document.getElementsByClassName("section-4-container-sub")[0];
 let s_container_scroll = document.getElementsByClassName("s-container-scroll")[0];
+let menu_btn = document.getElementsByClassName("menu-btn")[0];
+let navlist = document.getElementsByClassName("navlist")[0];
+let close = document.getElementsByClassName("close")[0];
 let checkscroll = false;
 
 left.addEventListener("click", () => {
-    scrollbar.scrollLeft = scrollbar.scrollLeft - 1360;
+    scrollbar.scrollLeft -= scrollbar.clientWidth;
 })
 
 right.addEventListener("click", () => {
-    scrollbar.scrollLeft = scrollbar.scrollLeft + 1360;
+    scrollbar.scrollLeft += scrollbar.clientWidth;
 })
 
-setInterval(() => {
-    if (scrollbar.scrollLeft <= 1360 && checkscroll == false) {
-        scrollbar.scrollLeft = scrollbar.scrollLeft + 1360;
-        if(scrollbar.scrollLeft == 1360){
-            checkscroll = true;
-        }
-        }
-    else{
-        scrollbar.scrollLeft = scrollbar.scrollLeft - 1360;
-        if(scrollbar.scrollLeft == 0){
-            checkscroll = false;
-        }
-    }
-}, 5000);
+// setInterval(() => {
+//     if (scrollbar.scrollLeft <= 1360 && checkscroll == false) {
+//         scrollbar.scrollLeft += scrollbar.clientWidth;
+//         if (scrollbar.scrollLeft == 1360) {
+//             checkscroll = true;
+//         }
+//     }
+//     else {
+//         scrollbar.scrollLeft -= scrollbar.clientWidth;
+//         if (scrollbar.scrollLeft == 0) {
+//             checkscroll = false;
+//         }
+//     }
+// }, 5000);
 
 left2.addEventListener("click", () => {
-    s_container.scrollLeft -= 280;
+    s_container.scrollLeft -= s_container.clientWidth/4;
 });
 
 right2.addEventListener("click", () => {
-    s_container.scrollLeft += 280;
+    s_container.scrollLeft += s_container.clientWidth/4;
 });
 
 left3.addEventListener("click", () => {
-    shopby_corncern.scrollLeft -= 1000;
+    shopby_corncern.scrollLeft -= shopby_corncern.clientWidth/4;
 })
 
 right3.addEventListener("click", () => {
-    shopby_corncern.scrollLeft += 1000;
+    shopby_corncern.scrollLeft += shopby_corncern.clientWidth/4;
 })
 
-left4.addEventListener("click",()=>{
-    section_4_container_sub.scrollLeft-=1310;
+left4.addEventListener("click", () => {
+    section_4_container_sub.scrollLeft -= section_4_container_sub.clientWidth/4;
 })
 
-right4.addEventListener("click",()=>{
-    section_4_container_sub.scrollLeft+=1310;
+right4.addEventListener("click", () => {
+    section_4_container_sub.scrollLeft += section_4_container_sub.clientWidth/4;
 })
 
-left5.addEventListener("click",()=>{
-    s_container_scroll.scrollLeft-=280;
+left5.addEventListener("click", () => {
+    s_container_scroll.scrollLeft -= s_container_scroll.clientWidth/4;
 })
 
-right5.addEventListener("click",()=>{
-    s_container_scroll.scrollLeft+=280;
+right5.addEventListener("click", () => {
+    s_container_scroll.scrollLeft += s_container_scroll.clientWidth/4;
+})
+
+//Event listener on Side bar
+
+menu_btn.addEventListener("click",()=>{
+    navlist.style.left = 0;
+})
+
+close.addEventListener("click",()=>{
+    navlist.style.left = "-115%";
 })
